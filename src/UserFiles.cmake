9f5e85a5a766974f10dc6b783fed4280985e6305
Dmitriy <zapadcenter@gmail.com>
2025-09-25 01:35:41 +0300

    Попытка переделать CMake - неудачная

---
+++ src/UserFiles.cmake
@@ -0,0 +1,12 @@
+    # include_directories(Arduino)
+    
+    # Список общих платфоремнно-назависимых файлов проекта
+    SET(ArduinoFiles
+        src/arduino_sketch.cpp
+    )
+
+        
+
+
+
+
